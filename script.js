749007ddc55bfe9267e6245802e4a77c1888e7f2
voidkuma <xmhernandez@ucdavis.edu>
2026-04-06 01:05:04 -0700

    firstHomeVerison

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 (function() {
     'use strict';
 
+    const classInfo = document.querySelector('#classInfo');
+    const bannerItem = document.querySelector('#movingItem');
     const button = document.querySelector('button');
+    const myName = document.querySelector('#myName');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
     const sections = document.querySelectorAll('section')
@@ -9,6 +12,9 @@
 
     button.addEventListener('click', function() {
         if (mode === 'dark') {
+            bannerItem.setAttribute('src', 'images/horse.png');
+            classInfo.className = 'switch';
+            myName.className = 'switch';
             body.className = 'switch';
             banner.className = 'switch';
             button.className = 'switch';
@@ -17,6 +23,10 @@
             }
             mode = 'light';
         } else {
+            
+            bannerItem.setAttribute('src', 'images/pirateShip.png');
+            classInfo.removeAttribute('class');
+            myName.removeAttribute('class');
             body.removeAttribute('class');
             banner.removeAttribute('class');
             button.removeAttribute('class');
