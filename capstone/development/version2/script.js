52e8add1f4462f3ccb39062a803400f2cea18542
voidkuma <xmhernandez@ucdavis.edu>
2026-05-28 14:41:25 -0700

    finishing basic layout to the questions & responses

--- capstone/development/version2/script.js
+++ capstone/development/version2/script.js
@@ -3,6 +3,8 @@
     console.log('reading js');
 
 
+    // [Logic]: User pressing play -> then leading onto the next scene
+
     const start = document.querySelector('#startBtn');
     const openingScene = document.querySelector('#openingScene');
     const question1 = document.querySelector('#question1');
@@ -13,7 +15,7 @@
 
     })
 
-    // logic behind user pressing first question answer to lead to next question
+    // [Logic]: user answering Question #1 and having that lead to next question
 
     const btns = document.querySelectorAll('#MCQ button');
     const reflection1 = document.querySelector('#reflectScene1');
